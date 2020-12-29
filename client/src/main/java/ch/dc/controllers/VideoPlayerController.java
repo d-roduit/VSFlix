e03c6ac7757464dba234e9c00a0b7c8bb877e266
d-roduit <daniel@roduit.com>
2020-12-29 15:03:59 +0100

    Fix mediaView layout to handle vertical and large landscape videos

--- client/src/main/java/ch/dc/controllers/VideoPlayerController.java
+++ client/src/main/java/ch/dc/controllers/VideoPlayerController.java
@@ -1,4 +1,20 @@
 package ch.dc.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.media.MediaView;
+
 public class VideoPlayerController {
+
+    @FXML
+    private BorderPane pageContentContainer;
+
+    @FXML
+    private MediaView mediaView;
+
+    @FXML
+    public void initialize() {
+        mediaView.fitWidthProperty().bind(pageContentContainer.widthProperty().multiply(0.7));
+        mediaView.fitHeightProperty().bind(pageContentContainer.heightProperty().multiply(0.6));
+    }
 }
